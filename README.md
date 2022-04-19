dfa40857faabd940c48d38ff5c948b97d8feb1a3
PavanMudigonda <mnpawan@gmail.com>
2022-04-19 11:35:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# JaCoCo Code Coverage Reporter
+
+GitHub Action to Publish JaCoCo Format Code Coverage XML and attach it
+to the Workflow Run as a Check Run. You can even set threshold coverage percentage and fail the action.
+
+### Note:- The scope of this project is limited to Report and Quality Gate. Any ideas are welcome. 
+###        If you like my Github Action, please **STAR ⭐** it.
+
+## Samples
+
+
+This Action allows you to specify your JaCoCo Code Coverage XML Path, and then
+generate a markdown report from the test results and then it attaches it
+to the Workflow Run as a Check Run. You can even set threshold coverage percentage and fail the action.
+
+Here's a quick example of how to use this action in your own GitHub Workflows.
+
+```yaml
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+    
+      # generates coverage-report.md and publishes as checkrun
+      - name: JaCoCo Code Coverage Report
+        id: jacoco_reporter
+        uses: PavanMudigonda/jacoco-reporter@v3.0
+        with:
+          coverage_results_path: jacoco-report/test.xml
+          coverage_report_name: Coverage
+          coverage_report_title: JaCoCo
+          github_token: ${{ secrets.GITHUB_TOKEN }}
+          skip_check_run: false
+          minimum_coverage: 80
+          fail_below_threshold: false
+          publish_only_summary: false
+          
+      # uploads the coverage-report.md artifact    
+      - name: Upload Code Coverage Artifacts
+        uses: actions/upload-artifact@v2
+        with:
+          name: code-coverage-report-markdown
+          path: */coverage-results.md 
+          retention-days: 1  
+```
+
+
+### Inputs
+
+This Action defines the following formal inputs.
+
+| Name | Req | Description
+|-|-|-|
+| **`coverage_results_path`**  | true | Path to the JaCoCo Code Coverage XML format file which will be used to generate a report. 
+| **`coverage_report_name`** | false | The name of the code coverage report object that will be attached to the Workflow Run.  Defaults to the name `COVERAGE_RESULTS_<datetime>` where `<datetime>` is in the form `yyyyMMdd_hhmmss`.
+| **`coverage_report_title`** | false | The title of the code coverage report that will be embedded in the report itself, which defaults to the same as the `coverage_report_name` input.
+|**`github_token`** | true | Input the GITHUB TOKEN Or Personal Access Token you would like to use. Recommended to use GitHub auto generated token ${{ secrets.GITHUB_TOKEN }}
+|**`minimum_coverage`** | false | Input the minimum code coverage recommended.
+|**`fail_below_threshold`** | false | Set True to fail the action and False to let it pass.
+|**`skip_check_run`** | false | If true, will skip attaching the Coverage Result report to the Workflow Run using a Check Run. Useful if your report has 65k characters that is not accepted by Github REST and GraphQL APIs
+|**`publish_only_summary`** | false | If true, will publish only a summary table of the Coverage Result report to the Workflow Run using a Check Run. Useful if your full coverage report has 65k characters that is not accepted by Github REST and GraphQL APIs
+
+### Outputs
+
+This Action defines the following formal outputs.
+
+| Name | Description
+|-|-|
+| **`coverage_percentage`** | Coverage Percentage
+| **`covered_lines`** | Total Covered Lines
+| **`missed_lines`** | Total missed Lines
+| **`total_lines`** | Total Code Lines
+| **`coverage_results_path`** | Path to the code coverage results file in JaCoCo XML format.
+
+### Sample Screenshot (Full Coverage Report): publish_only_summary: false
+
+![image](https://user-images.githubusercontent.com/29324338/155446462-023a310a-c353-4a4c-9b3c-d25e7862ee74.png)
+
+
+### Sample Screenshot (Summary Coverage Report): publish_only_summary: true
+
+![image](https://user-images.githubusercontent.com/29324338/163588129-fbc94144-01b5-4af5-81ad-91a1e22a8c5d.png)
+
+
+### Sample Repo 
+
+https://github.com/PavanMudigonda/jacoco-playground
+
+### Sample Github Actions workflow 
+
+https://github.com/PavanMudigonda/jacoco-playground/blob/main/.github/workflows/coverage.yml
+
+
+### PowerShell GitHub Action
+
+This Action is implemented as a [PowerShell GitHub Action](https://github.com/ebekker/pwsh-github-action-base).
---
+++ action.ps1
@@ -0,0 +1,206 @@
+#!/usr/bin/env pwsh
+
+$ErrorActionPreference = 'Stop'
+
+## Make sure any modules we depend on are installed
+$modulesToInstall = @(
+    'GitHubActions'
+)
+
+$modulesToInstall | ForEach-Object {
+    if (-not (Get-Module -ListAvailable -All $_)) {
+        Write-Output "Module [$_] not found, INSTALLING..."
+        Install-Module $_ -Force
+    }
+}
+
+## Import dependencies
+Import-Module GitHubActions -Force
+
+Write-ActionInfo "Running from [$($PSScriptRoot)]"
+
+function splitListInput { $args[0] -split ',' | % { $_.Trim() } }
+function writeListInput { $args[0] | % { Write-ActionInfo "    - $_" } }
+
+$inputs = @{
+    coverage_report_name = Get-ActionInput coverage_report_name
+    coverage_report_title = Get-ActionInput coverage_report_title
+    coverage_results_path = Get-ActionInput coverage_results_path -Required
+    github_token       = Get-ActionInput github_token -Required
+    skip_check_run     = Get-ActionInput skip_check_run
+    minimum_coverage   = Get-ActionInput minimum_coverage
+    fail_below_threshold = Get-ActionInput fail_below_threshold
+    publish_only_summary = Get-ActionInput publish_only_summary
+}
+
+$test_results_dir = Join-Path $PWD _TMP
+Write-ActionInfo "Creating test results space"
+mkdir $test_results_dir
+Write-ActionInfo $test_results_dir
+$script:coverage_report_path = Join-Path $test_results_dir coverage-results.md
+$script:publish_only_summary = $inputs.publish_only_summary
+$script:skip_check_run = $inputs.skip_check_run
+
+function Build-CoverageReport 
+{
+    Write-ActionInfo "Building human-readable code-coverage report"
+    $script:coverage_report_name = $inputs.coverage_report_name
+    $script:coverage_report_title = $inputs.coverage_report_title
+    $script:coverage_results_path = $inputs.coverage_results_path
+
+    if (-not $script:coverage_report_name) {
+        $script:coverage_report_name = "COVERAGE_RESULTS_$([datetime]::Now.ToString('yyyyMMdd_hhmmss'))"
+    }
+    if (-not $coverage_report_title) {
+        $script:coverage_report_title = $report_name
+    }
+
+        $script:coverage_report_path = Join-Path $test_results_dir coverage-results.md
+        & "$PSScriptRoot/jacoco-report/jacocoxml2md.ps1" -Verbose `
+            -xmlFile $script:coverage_results_path `
+            -mdFile $script:coverage_report_path -xslParams @{
+                reportTitle = $script:coverage_report_title
+            }
+    
+        & "$PSScriptRoot/jacoco-report/embedmissedlines.ps1" -mdFile $script:coverage_report_path
+    
+}
+
+function Build-CoverageSummaryReport 
+{
+    Write-ActionInfo "Building human-readable code-coverage report"
+    $script:coverage_report_name = $inputs.coverage_report_name
+    $script:coverage_report_title = $inputs.coverage_report_title
+    $script:coverage_results_path = $inputs.coverage_results_path
+
+    if (-not $script:coverage_report_name) {
+        $script:coverage_report_name = "COVERAGE_RESULTS_$([datetime]::Now.ToString('yyyyMMdd_hhmmss'))"
+    }
+    if (-not $coverage_report_title) {
+        $script:coverage_report_title = $report_name
+    }
+
+    $script:coverage_report_path = Join-Path $test_results_dir coverage-results.md
+    & "$PSScriptRoot/jacoco-report/jacocoxmlsummary2md.ps1" -Verbose `
+        -xmlFile $script:coverage_results_path `
+        -mdFile $script:coverage_report_path -xslParams @{
+            reportTitle = $script:coverage_report_title
+        }
+}
+
+function Publish-ToCheckRun {
+    param(
+        [string]$reportData,
+        [string]$reportName,
+        [string]$reportTitle
+    )
+
+    Write-ActionInfo "Publishing Report to GH Workflow"
+
+    $ghToken = $inputs.github_token
+    $ctx = Get-ActionContext
+    $repo = Get-ActionRepo
+    $repoFullName = "$($repo.Owner)/$($repo.Repo)"
+
+    Write-ActionInfo "Resolving REF"
+    $ref = $ctx.Sha
+    if ($ctx.EventName -eq 'pull_request') {
+        Write-ActionInfo "Resolving PR REF"
+        $ref = $ctx.Payload.pull_request.head.sha
+        if (-not $ref) {
+            Write-ActionInfo "Resolving PR REF as AFTER"
+            $ref = $ctx.Payload.after
+        }
+    }
+    if (-not $ref) {
+        Write-ActionError "Failed to resolve REF"
+        exit 1
+    }
+    Write-ActionInfo "Resolved REF as $ref"
+    Write-ActionInfo "Resolve Repo Full Name as $repoFullName"
+
+    Write-ActionInfo "Adding Check Run"
+    $url = "https://api.github.com/repos/$repoFullName/check-runs"
+    $hdr = @{
+        Accept = 'application/vnd.github.antiope-preview+json'
+        Authorization = "token $ghToken"
+    }
+    $bdy = @{
+        name       = $reportName
+        head_sha   = $ref
+        status     = 'completed'
+        conclusion = 'neutral'
+        output     = @{
+            title   = $reportTitle
+            summary = "This run completed at ``$([datetime]::Now)``"
+            text    = $ReportData
+        }
+    }
+    Invoke-WebRequest -Headers $hdr $url -Method Post -Body ($bdy | ConvertTo-Json)
+}
+
+
+Write-ActionInfo "Publishing Report to GH Workflow"    
+$coverage_results_path = $inputs.coverage_results_path
+if ($inputs.skip_check_run -ne $true -and $inputs.publish_only_summary -eq $true )
+    {
+        Build-CoverageSummaryReport
+        
+        $coverageSummaryData = [System.IO.File]::ReadAllText($coverage_report_path)
+        
+        Publish-ToCheckRun -ReportData $coverageSummaryData -ReportName $coverage_report_name -ReportTitle $coverage_report_title
+    }
+elseif ($inputs.skip_check_run -ne $true -and $inputs.publish_only_summary -ne $true )
+    {
+        Build-CoverageReport
+        
+        $coverageSummaryData = [System.IO.File]::ReadAllText($coverage_report_path)
+        
+        Publish-ToCheckRun -ReportData $coverageSummaryData -ReportName $coverage_report_name -ReportTitle $coverage_report_title
+    }
+elseif ($inputs.skip_check_run -eq $true -and $inputs.publish_only_summary -eq $true )
+    {
+    
+        Build-CoverageSummaryReport
+        
+    }
+else {
+
+        Build-CoverageReport
+        
+    }
+$coverageXmlData = Select-Xml -Path $coverage_results_path -XPath "/report/counter[@type='LINE']"
+$coveredLines = $coverageXmlData.Node.covered
+Write-Host "Covered Lines: $coveredLines"
+$missedLines = $coverageXmlData.Node.missed
+Write-Host "Missed Lines: $missedLines"
+if ($missedLines -eq 0) 
+    {
+    $coveragePercentage = 100
+    } 
+else 
+    {
+    $coveragePercentage = [math]::Round(100 - (($missedLines / $coveredLines) * 100))
+    }
+$coveragePercentageString = "$coveragePercentage%"
+Write-Output $coveragePercentageString
+Set-ActionOutput -Name coveragePercentage -Value $coveragePercentage
+$script:coverage_value = $coveragePercentage
+Set-ActionOutput -Name coverage_results_path -Value $coverage_results_path
+Set-ActionOutput -Name coverage_percentage -Value ($coveragePercentage)
+Set-ActionOutput -Name covered_lines -Value ($coveredLines)
+Set-ActionOutput -Name missed_lines -Value ($missedLines)
+Set-ActionOutput -Name total_lines -Value ($coveredLines+$missedLines)
+
+if ($inputs.fail_below_threshold -eq "true") {
+        Write-ActionInfo "  * fail_below_threshold: true"
+    }
+    
+if ($coverage_value -lt $inputs.minimum_coverage -and $inputs.fail_below_threshold -eq "true") {
+        $script:stepShouldFail = $true
+    }
+
+if ($stepShouldFail) {
+    Write-ActionInfo "Thowing error as Code Coverage is less than "minimum_coverage" is not met and 'fail_below_threshold' was true."
+    throw "Code Coverage is less than Minimum Code Coverage Required"
+}
